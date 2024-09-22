864ad5bbf891b010738d89973f9ecd342af5e83e
prerakkaushik <prerak.kaushik@gmail.com>
2024-09-22 13:43:52 -0400

    upsert operation to update "last_login_at" time.

--- models/user_dto.py
+++ models/user_dto.py
@@ -11,3 +11,23 @@ class UserDto:
     user_id: Optional[str] = None
     created_at: Optional[datetime] = None
     last_login_at: Optional[datetime] = None
+
+
+    def to_dict(self, include_none=False):
+        """Return the object's attributes as a dictionary."""
+        # Get all attributes as a dictionary
+        attributes = {
+            "azure_ad_id": self.azure_ad_id,
+            "tenant_id": self.tenant_id,
+            "email": self.email,
+            "full_name": self.full_name,
+            "user_id": self.user_id,
+            "created_at": self.created_at,
+            "last_login_at": self.last_login_at,
+        }
+        
+        # If include_none is False, filter out None values
+        if not include_none:
+            attributes = {k: v for k, v in attributes.items() if v is not None}
+        
+        return attributes
--- services/user_svc.py
+++ services/user_svc.py
@@ -1,5 +1,5 @@
 import datetime
-from sqlalchemy import insert
+from sqlalchemy.dialects.postgresql import insert
 from commands.db import getSession
 from models.user_dto import UserDto
 from models.user import DbUser
@@ -7,9 +7,18 @@ from models.user import DbUser
 async def RegisterUser(userInfo: UserDto) -> bool:
     async with getSession() as connection_db_user:
         try:
-            db_user = DbUser(azure_ad_id=userInfo.azure_ad_id, tenant_id=userInfo.tenant_id, email=userInfo.email, full_name=userInfo.full_name, last_login_at=datetime.datetime.now(datetime.timezone.utc))
-            connection_db_user.add(db_user)
+            #db_user = DbUser(azure_ad_id=userInfo.azure_ad_id, tenant_id=userInfo.tenant_id, email=userInfo.email, full_name=userInfo.full_name, last_login_at=datetime.datetime.now(datetime.timezone.utc))
+            # connection_db_user.add(db_user)
+            # connection_db_user.commit()
+
+            # Create an update/insert statement for the "Users" table
+            stmt = insert(DbUser).values(userInfo.to_dict(include_none=False)).on_conflict_do_update(
+                index_elements=['azure_ad_id', 'tenant_id'],  # the unique constraint or index for conflict resolution
+                set_={'last_login_at': datetime.datetime.now(datetime.timezone.utc)}  # Update specific fields on conflict
+            )
+            connection_db_user.execute(stmt)
             connection_db_user.commit()
+            return True
         except Exception as error:
             print('Error while RegisterUser method', error)
     return False
